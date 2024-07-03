1cdeebd23e0ed957914ab7132726160496d59447
Sergey Parshin <s.parshin@dcoa.ru>
2024-07-03 13:27:46 +0300

    HW6 first try

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,11 @@
 package hw06pipelineexecution
 
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -10,5 +16,31 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
-	return nil
+	var stop atomic.Bool
+	start := make(Bi)
+	go func() {
+		start <- struct{}{}
+		_, ok := <-done
+		if !ok {
+			t := time.Now()
+			fmt.Printf("%t\n", ok)
+			fmt.Println(t.Format("15:04:05.000"))
+			stop.CompareAndSwap(false, true)
+		}
+	}()
+	<-start
+	return f(stages, in, &stop)
+}
+
+func f(stages []Stage, in In, stop *atomic.Bool) Out {
+	for i := 0; i < len(stages); i++ {
+		t := time.Now()
+		fmt.Println(t.Format("15:04:05.000"))
+		fmt.Printf("stop %t\n", stop.Load())
+		if stop.Load() {
+			return in
+		}
+		in = stages[i](in)
+	}
+	return in
 }
