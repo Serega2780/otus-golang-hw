4045f6ca2c340135c11779f6ee74b3f7379f95ec
Sergey Parshin <s.parshin@dcoa.ru>
2024-07-18 16:47:17 +0300

    HW7 try 1

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,116 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sync"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrFileNotFound          = errors.New("file not found")
+	ErrFileStat              = errors.New("file stat error")
+	ErrOpenFile              = errors.New("file open error")
+	ErrReadFile              = errors.New("file read error")
+	ErrWriteFile             = errors.New("file write error")
+	N                        = 1024
+	inF                      *os.File
+	outF                     *os.File
+	buf                      []byte
+	ch                       chan int64
+	wg                       sync.WaitGroup
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	ch = make(chan int64, 1)
+	if limit == 0 {
+		limit = math.MaxInt64
+	}
+
+	if limit < int64(N) {
+		buf = make([]byte, limit)
+	} else {
+		buf = make([]byte, N)
+	}
+
+	var size int64
+
+	defer inF.Close()
+	defer outF.Close()
+
+	err := check(fromPath, toPath, offset, &size)
+	if err != nil {
+		return err
+	}
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		for d := range ch {
+			fmt.Printf(" %d"+"%%", 100*d/size)
+		}
+		fmt.Print(" 100%")
+	}(&wg)
+
+	wOffset := int64(0)
+	var fErr error
+	var n int
+	for wOffset < limit && fErr != io.EOF {
+		n, fErr = inF.ReadAt(buf, offset)
+		if fErr != nil && fErr != io.EOF {
+			return fmt.Errorf(ErrReadFile.Error()+" "+fErr.Error()+" %s\n", fromPath)
+		}
+		tmp := wOffset + int64(n)
+		if tmp > limit {
+			buf = buf[0 : limit-wOffset]
+		} else if fErr == io.EOF {
+			buf = buf[0:n]
+		}
+
+		n, err = outF.WriteAt(buf, wOffset)
+		if err != nil {
+			return fmt.Errorf(ErrWriteFile.Error()+" "+err.Error()+" %s\n", toPath)
+		}
+		wOffset += int64(n)
+		offset += int64(n)
+		ch <- wOffset
+	}
+	close(ch)
+	wg.Wait()
+
+	return nil
+}
+
+func check(fromPath, toPath string, offset int64, size *int64) error {
+	var err error
+	inF, err = os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf(ErrFileNotFound.Error()+" "+err.Error()+" %s\n", fromPath)
+		}
+		return fmt.Errorf(ErrOpenFile.Error()+" "+err.Error()+" %s\n", fromPath)
+	}
+
+	inFInf, er := inF.Stat()
+	if er != nil {
+		return fmt.Errorf(ErrFileStat.Error()+" "+er.Error()+" %s\n", fromPath)
+	}
+	*size = inFInf.Size()
+	if *size == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset >= inFInf.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	outF, err = os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf(ErrOpenFile.Error()+" "+err.Error()+" %s\n", toPath)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,127 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	buf, buf2 := make([]byte, 1024*1024), make([]byte, 1024*1024)
+
+	t.Run("offset0_limit0 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 0, 0)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/out_offset0_limit0.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("offset0_limit10 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 0, 10)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/out_offset0_limit10.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("offset0_limit1000 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 0, 1000)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/out_offset0_limit1000.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("offset0_limit10000 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 0, 10000)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/out_offset0_limit10000.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("offset100_limit1000 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 100, 1000)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/out_offset100_limit1000.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("offset6000_limit1000 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 6000, 1000)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/out_offset6000_limit1000.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("input_one_symbol case", func(t *testing.T) {
+		err := Copy(`./testdata/input_one_symbol.txt`, `/tmp/random`, 0, 0)
+		require.NoError(t, err, nil)
+
+		f, _ := os.Open(`/tmp/random`)
+		f2, _ := os.Open(`./testdata/input_one_symbol.txt`)
+		defer f.Close()
+		defer f2.Close()
+
+		_, _ = f.ReadAt(buf, 0)
+		_, _ = f2.ReadAt(buf2, 0)
+		require.True(t, testEq(buf, buf2))
+	})
+
+	t.Run("offset10000_limit0 case", func(t *testing.T) {
+		err := Copy(`./testdata/input.txt`, `/tmp/random`, 10000, 0)
+		require.Error(t, err, ErrOffsetExceedsFileSize)
+	})
+}
+
+func testEq(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
