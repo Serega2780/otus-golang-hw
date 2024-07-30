3f3fa39f6c29c24e4de2dab9e26cc56ad1c3928e
Sergey Parshin <s.parshin@dcoa.ru>
2024-07-30 17:00:49 +0300

    HW8 try 1

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,50 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	// Place your code here.
-	return
+	var exitCodeErr *exec.ExitError
+
+	if err := setEnv(&env); err != nil {
+		fmt.Println(err.Error())
+		return -1
+	}
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	command.Stdin = os.Stdin
+	command.Stderr = os.Stderr
+	command.Stdout = os.Stdout
+	err := command.Run()
+	if err != nil {
+		ok := errors.As(err, &exitCodeErr)
+		if ok {
+			fmt.Println(err.Error())
+			return exitCodeErr.ExitCode()
+		}
+		fmt.Println(err.Error())
+		return -1
+	}
+
+	return 0
+}
+
+func setEnv(env *Environment) error {
+	for k, v := range *env {
+		if v.NeedRemove {
+			if err := os.Unsetenv(k); err != nil {
+				return err
+			}
+		} else {
+			if err := os.Setenv(k, v.Value); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("usage: envdir dir child")
+		return
+	}
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	RunCmd(os.Args[2:], env)
 }
