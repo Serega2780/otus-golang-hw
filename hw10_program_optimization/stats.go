f35e2c8c0a14d14ba3b21df95502c8cefdaacce3
Sergey Parshin <s.parshin@dcoa.ru>
2024-08-07 13:22:08 +0300

    HW10 try 1 optimization with benchstat results

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,17 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
+	"sync"
+
+	"github.com/goccy/go-json"
+)
+
+const (
+	EmptyString = ""
 )
 
 type User struct {
@@ -20,47 +26,52 @@ type User struct {
 
 type DomainStat map[string]int
 
+var userPool = sync.Pool{
+	New: func() interface{} { return new(User) },
+}
+
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
+	domain = "." + domain
+	result, err := processData(r, domain)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
-	return countDomains(u, domain)
+
+	return result, nil
 }
 
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
+func processData(r io.Reader, domain string) (result DomainStat, err error) {
+	result = make(DomainStat)
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		content := scanner.Bytes()
+		user := userPool.Get().(*User)
+		if err = json.Unmarshal(content, user); err != nil {
+			continue
+		}
+		countDomains(*user, domain, &result)
+		user.resetUser()
+		userPool.Put(user)
+	}
+	if err = scanner.Err(); err != nil {
 		return
 	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
+	return result, nil
 }
 
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
+func countDomains(user User, domain string, result *DomainStat) {
+	key := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
+	if strings.Contains(user.Email, domain) {
+		(*result)[key]++
 	}
-	return result, nil
+}
+
+func (user *User) resetUser() {
+	user.ID = 0
+	user.Name = EmptyString
+	user.Username = EmptyString
+	user.Email = EmptyString
+	user.Phone = EmptyString
+	user.Password = EmptyString
+	user.Address = EmptyString
 }
