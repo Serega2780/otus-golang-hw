b487880fa24a0fce333f4a781037469bb075a378
Sergey Parshin <s.parshin@dcoa.ru>
2024-08-20 13:03:40 +0300

    HW11 try 1 simple telnet client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,88 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	TimeoutFlag  = "timeout"
+	TimeoutValue = 10
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Duration(TimeoutValue*float64(time.Second)), "timeout to connect to server")
+}
+
 func main() {
 	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	// P.S. Do not rush to throw context down, think if it is useful with blocking operation?
+	stop := make(chan interface{})
+	var host, port string
+	arguments := os.Args
+	flag.Parse()
+
+	if (isFlagPassed(TimeoutFlag) && len(arguments) < 4) || (!isFlagPassed(TimeoutFlag) && len(arguments) < 3) {
+		fmt.Println("Please provide host port as separate arguments")
+		return
+	}
+	if isFlagPassed(TimeoutFlag) {
+		host = os.Args[2]
+		port = os.Args[3]
+	} else {
+		host = os.Args[1]
+		port = os.Args[2]
+	}
+
+	tc := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
+	if err := tc.Connect(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	go func(tc TelnetClient) {
+		for {
+			_ = tc.Receive()
+		}
+	}(tc)
+
+	go func(tc TelnetClient) {
+		for {
+			err := tc.Send()
+			if err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, err)
+			}
+		}
+	}(tc)
+
+	go func() {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, os.Interrupt, syscall.SIGTERM, syscall.SIGURG)
+		sig := <-ch
+		if sig == syscall.SIGURG {
+			_, _ = fmt.Fprintln(os.Stderr, "...EOF")
+			if err := tc.Close(); err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		os.Exit(1)
+	}()
+
+	<-stop
+}
+
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
+	"os"
+	"strings"
 	"time"
+)
+
+const (
+	TCP   = "tcp"
+	COLON = ":"
 )
 
 type TelnetClient interface {
@@ -12,8 +21,54 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TC struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
+	return &TC{address: address, timeout: timeout, in: in, out: out}
+}
+
+func (tc *TC) Connect() error {
+	server := strings.Split(tc.address, COLON)[0]
+	_, _ = fmt.Fprintln(os.Stderr, "Trying "+server+"...")
+	dialer := net.Dialer{Timeout: tc.timeout, KeepAlive: time.Duration(5 * float64(time.Second))}
+	conn, err := dialer.Dial(TCP, tc.address)
+	if err == nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Connected to %s.\n", server)
+		_, _ = fmt.Fprintln(os.Stderr, "Press 'Ctrl+D or Ctrl+C' to quit")
+		tc.conn = conn
+	}
+	return err
+}
+
+func (tc *TC) Close() error {
+	_, _ = fmt.Fprintln(os.Stderr, "Closing connection... ")
+	if err := tc.conn.Close(); err != nil {
+		return err
+	}
+	_, _ = fmt.Fprintln(os.Stderr, "Exited.")
+	return nil
+}
+
+func (tc *TC) Send() error {
+	_, err := io.Copy(tc.conn, tc.in)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tc *TC) Receive() error {
+	_, err := io.Copy(tc.out, tc.conn)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
